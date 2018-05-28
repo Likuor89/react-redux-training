3f3c05a870914ac5f61c5917f2cea82d77480f6e
Danil__Leonidov <jibing89@gmail.com>
2018-05-28 16:09:45 +0300

    1

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -6,7 +6,7 @@ import About from '../about'
 const App = () => (
     <div className="wrap">
       <header>
-        <Link to="/">Home</Link>
+        <Link to="/">Home-test</Link>
         <Link to="/about-us">About</Link>
       </header>
       
